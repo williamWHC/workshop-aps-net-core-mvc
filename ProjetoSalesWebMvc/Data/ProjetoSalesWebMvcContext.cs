905aca1d6cbd8dbabf4d29297f216f99fe49001e
William Henrique <williamhenriquecirino@hotmail.com>
2019-11-12 18:16:30 -0300

    CRUD Scaffolding

---
+++ ProjetoSalesWebMvc/Data/ProjetoSalesWebMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoSalesWebMvc.Models
+{
+    public class ProjetoSalesWebMvcContext : DbContext
+    {
+        public ProjetoSalesWebMvcContext (DbContextOptions<ProjetoSalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoSalesWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- ProjetoSalesWebMvc/Startup.cs
+++ ProjetoSalesWebMvc/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using ProjetoSalesWebMvc.Models;
 
 namespace ProjetoSalesWebMvc
 {
@@ -24,6 +26,9 @@ namespace ProjetoSalesWebMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ProjetoSalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoSalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
